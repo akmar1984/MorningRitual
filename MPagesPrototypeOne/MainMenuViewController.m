edcd86585dedbc75254ea9df9b8d15d2c50851d9
Marek Tomaszewski <akmar@Mareks-MacBook-Air.local>
2015-10-12 15:02:13 +0100

    Implemented the video in the background on the main Screen

--- MPagesPrototypeOne/MainMenuViewController.m
+++ MPagesPrototypeOne/MainMenuViewController.m
@@ -12,20 +12,62 @@
 #import "BFPaperButton.h"
 #import "UIColor+BFPaperColors.h"
 #import "FontAwesomeKit.h"
-
+#import <AVFoundation/AVFoundation.h>
+@import MediaPlayer;
 @interface MainMenuViewController ()
 @property (nonatomic)BFPaperButton *flatRoundedButton;
-
+@property (weak, nonatomic) IBOutlet UIImageView *titleImageView;
+@property (nonatomic, strong)MPMoviePlayerController *moviePlayer;
 @end
-@implementation MainMenuViewController
+@implementation MainMenuViewController{
+    
+    AVPlayerLayer *_playerLayer;
+    UIView *_playerView;
+}
 -(void)viewWillAppear:(BOOL)animated{
     
     [super viewWillAppear:animated];
 
+    [self setTheVideo];
+    
     [self setAnimatedButton];
     
 }
+-(void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
 
+}
+-(void)setTheVideo{
+    NSString *path = [[NSBundle mainBundle]pathForResource:@"flower2" ofType:@"m4v"]; // flower2 720x576
+    NSURL *url = [NSURL fileURLWithPath:path];
+    
+    _playerLayer =[AVPlayerLayer playerLayerWithPlayer:[[AVPlayer alloc]initWithURL:url]];
+    self.view.backgroundColor = [UIColor clearColor];
+    
+    _playerView = [[UIView alloc]initWithFrame:self.view.bounds];
+    _playerView.backgroundColor = [UIColor redColor];
+    
+    _playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    [self.view insertSubview:_playerView atIndex:1];
+    [_playerView.layer addSublayer:_playerLayer];
+    _playerLayer.player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
+    [_playerLayer.player play];
+    _playerLayer.frame = _playerView.layer.bounds;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loopVideo) name:AVPlayerItemDidPlayToEndTimeNotification object:_playerLayer.player];
+}
+
+-(void)viewDidLoad{
+
+
+    
+    
+}
+
+- (void)loopVideo {
+    [_playerLayer.player play];
+
+}
 -(void)setAnimatedButton{
     CGPoint middleScreen = CGPointMake(roundf(self.view.bounds.size.width - 50)/2.0, roundf(self.view.bounds.size.height -50)/2.0);
     self.flatRoundedButton = [[BFPaperButton alloc]initWithFrame:CGRectMake(middleScreen.x, middleScreen.y, 50, 50) raised:NO];
@@ -59,9 +101,9 @@
 -(IBAction)tappedButton:(id)sender{
     
    
-    
+    [_playerLayer.player play];
 
-    [self performSelector:@selector(delaySegue) withObject:nil afterDelay:0.3];
+    //[self performSelector:@selector(delaySegue) withObject:nil afterDelay:0.3];
    
 
 }
